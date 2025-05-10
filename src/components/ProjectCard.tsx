2725a3dd978463d55304a406506f12a1c04a7b6a
suny-saja <anak.guru.anak@gmail.com>
2025-05-10 07:34:03 +0700

    editing

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import projectList from '../data/project';
+
+type Props = {
+  project: (typeof projectList)[number];
+};
+
+export default function ProjectCard({ project }: Props) {
+  return (
+    <Link href={project.link} target='_blank' rel='noopener noreferrer'>
+      <div className='flex h-96 w-full flex-col gap-8 overflow-hidden rounded-2xl bg-white p-4 shadow-xl transition duration-300 hover:scale-105 hover:shadow-xl'>
+        <div className='flex-shrink-0'>
+          <Image
+            src={project.image}
+            alt={project.title}
+            className='h-48 w-full rounded-3xl object-cover'
+            width={400}
+            height={400}
+            loading='lazy'
+            placeholder='blur'
+            blurDataURL='/thumbnails/zul.png'
+            sizes='(max-width: 640px) 100vw, (max-width: 768px) 50vw, (max-width: 1024px) 33vw, (max-width: 1280px) 25vw, (min-width: 1281px) 20vw'
+            quality={100}
+          />
+        </div>
+        <div className='flex-grow p-2'>
+          <h2 className='text-xl font-semibold text-gray-800'>
+            0{project.id} -
+            <span className='text-blue-600'>{project.title}</span>
+          </h2>
+          <h3 className='text-sm text-gray-500'>{project.subtitle}</h3>
+          <p className='text-sm text-wrap text-gray-700'>
+            {project.description}
+          </p>
+        </div>
+      </div>
+    </Link>
+  );
+}
---
+++ src/data/project.ts
@@ -0,0 +1,101 @@
+export interface Project {
+  id: number;
+  title: string;
+  subtitle: string;
+  description: string;
+  image: string;
+  link: string;
+}
+
+export const projectList = [
+  {
+    id: 1,
+    title: 'Portfolio By Zul',
+    subtitle: 'Variant Porto 2',
+    description: 'This is the best portfolio from WPH bootcamp students.',
+    image: '/thumbnails/zul.png',
+    link: 'https://first-porto-zul.vercel.app',
+  },
+  {
+    id: 2,
+    title: 'Portfolio By Yana',
+    subtitle: 'Variant Porto 5',
+    description: 'This is the best portfolio from WPH bootcamp students.',
+    image: '/thumbnails/yana.png',
+    link: 'https://yanasoup-portfolio5.vercel.app',
+  },
+  {
+    id: 3,
+    title: 'Portfolio By Budi Hakim',
+    subtitle: 'Variant Porto 5',
+    description: 'This is the best portfolio from WPH bootcamp students.',
+    image: '/thumbnails/budi.png',
+    link: 'https://portofolio-liard-six.vercel.app',
+  },
+  {
+    id: 4,
+    title: 'Portfolio By Dwi Hermawati',
+    subtitle: 'Variant Porto 2',
+    description: 'This is the best portfolio from WPH bootcamp students.',
+    image: '/thumbnails/dwi.png',
+    link: 'https://lp-portfolio-bice.vercel.app',
+  },
+  {
+    id: 5,
+    title: 'Portfolio By Surya',
+    subtitle: 'Variant Porto 2',
+    description: 'This is the best portfolio from WPH bootcamp students.',
+    image: '/thumbnails/surya.png',
+    link: 'https://portfolio2-ub79.vercel.app',
+  },
+  {
+    id: 6,
+    title: 'Portfolio By Singgih',
+    subtitle: 'Variant Porto 5',
+    description: 'This is the best portfolio from WPH bootcamp students.',
+    image: '/thumbnails/singgih.png',
+    link: 'https://sdwianto-portfolio1.vercel.app',
+  },
+  {
+    id: 7,
+    title: 'Portfolio By',
+    subtitle: 'Variant Porto',
+    description: 'This is the best portfolio from WPH bootcamp students.',
+    image: '/thumbnails/zul.png',
+    link: 'https://example.com/project-7',
+  },
+  {
+    id: 8,
+    title: 'Portfolio By',
+    subtitle: 'Variant Porto',
+    description: 'This is the best portfolio from WPH bootcamp students.',
+    image: '/thumbnails/zul.png',
+    link: 'https://example.com/project-8',
+  },
+  {
+    id: 9,
+    title: 'Portfolio By',
+    subtitle: 'Variant Porto',
+    description: 'This is the best portfolio from WPH bootcamp students.',
+    image: '/thumbnails/zul.png',
+    link: 'https://example.com/project-9',
+  },
+  {
+    id: 10,
+    title: 'Portfolio By',
+    subtitle: 'Variant Porto',
+    description: 'This is the best portfolio from WPH bootcamp students.',
+    image: '/thumbnails/zul.png',
+    link: 'https://example.com/project-10',
+  },
+];
+
+export default projectList;
+// export const projects: Project[] = Array.from({ length: 20 }, (_, i) => ({
+//   id: i + 1,
+//   title: `Project ${i + 1}`,
+//   subtitle: `Subtitle for Project ${i + 1}`,
+//   description: `This is a brief description of project number ${i + 1}. It showcases creativity, collaboration, and technology.`,
+//   image: `https://via.placeholder.com/300x200?text=Project+${i + 1}`,
+//   link: `https://${id.projectList}`,
+// }));
