b8ad0c1f7916e9a46d8c180ed7995eb3f77c9563
suny-saja <anak.guru.anak@gmail.com>
2025-05-16 05:49:34 +0700

    refactor: restructure page layout and improve navbar functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,65 +7,67 @@ import { projectList } from '../data/project';
 
 export default function Page() {
   return (
-    <main className='custom-container relative overflow-hidden bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 px-4'>
+    <>
       <Navbar />
-      <div className='relative z-20'>
-        <div className='relative z-40 flex h-screen flex-col items-center justify-center text-center md:mt-2'>
-          <h1 className='mb-6 text-center text-3xl font-bold text-red-500 sm:mb-8 sm:text-4xl'>
-            Welcome to <br />
-            <span className='text-white'>SHOWZONE WPH BATCH 2</span>
-          </h1>
-          <div
-            id='about'
-            className='mb-8 text-center text-base text-gray-800 sm:mb-12 sm:text-2xl'
-          >
-            This is a showcase of our projects from the WPH Bootcamp. <br />
-            We are a group of passionate developers who have come together to
-            create amazing things. <br />
-            <br />
-            This is a collection of our projects, showcasing our skills and
-            creativity. <br />
-            We are excited to share our work with you and hope you enjoy it as
-            much as we enjoyed creating it. <br />
-            <br />
-            <span className='text-white'>Explore our projects</span> and
-            discover the talent and dedication of our team. <br />
-            <br />
-            We are always looking for new challenges and opportunities to grow
-            as developers. <br />
-            If you have any questions or would like to collaborate, please feel
-            free to reach out to us. <br />
-            <br />
-            Thank you for visiting our showcase! <br />
-            <br />
-            <span className='relative mb-2 text-white'>Happy exploring!</span>
-          </div>
-        </div>
-        <div
-          id='projects'
-          className='max-w-auto relative z-50 m-4 mt-2 grid grid-cols-1 gap-6 px-4 sm:m-8 sm:mt-10 sm:grid-cols-2 sm:gap-8 sm:px-8 md:grid-cols-3 lg:grid-cols-4'
-        >
-          {projectList.map((project, index) => (
+      <main className='custom-container relative top-0 z-1 overflow-hidden bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 px-4'>
+        <div className='relative'>
+          <div className='relative flex h-screen flex-col items-center justify-center text-center md:mt-2'>
+            <h1 className='mb-6 text-center text-3xl font-bold text-red-500 sm:mb-8 sm:text-4xl'>
+              Welcome to <br />
+              <span className='text-white'>SHOWZONE WPH BATCH 2</span>
+            </h1>
             <div
-              key={project.id}
-              className={`${
-                index >= projectList.length - 2
-                  ? 'col-span-1 flex justify-center sm:col-span-2'
-                  : 'items-center'
-              }`}
+              id='about'
+              className='text-center text-base text-gray-800 sm:text-2xl'
             >
-              <ProjectCard project={project} />
+              This is a showcase of our projects from the WPH Bootcamp. <br />
+              We are a group of passionate developers who have come together to
+              create amazing things. <br />
+              <br />
+              This is a collection of our projects, showcasing our skills and
+              creativity. <br />
+              We are excited to share our work with you and hope you enjoy it as
+              much as we enjoyed creating it. <br />
+              <br />
+              <span className='text-white'>Explore our projects</span> and
+              discover the talent and dedication of our team. <br />
+              <br />
+              We are always looking for new challenges and opportunities to grow
+              as developers. <br />
+              If you have any questions or would like to collaborate, please
+              feel free to reach out to us. <br />
+              <br />
+              Thank you for visiting our showcase! <br />
+              <br />
+              <span className='relative text-white'>Happy exploring!</span>
             </div>
-          ))}
+          </div>
+          <div
+            id='projects'
+            className='max-w-auto relative grid grid-cols-1 gap-6 px-4 sm:m-8 sm:grid-cols-2 sm:gap-8 sm:px-8 md:grid-cols-3 lg:grid-cols-4'
+          >
+            {projectList.map((project, index) => (
+              <div
+                key={project.id}
+                className={`${
+                  index >= projectList.length - 2
+                    ? 'col-span-1 flex justify-center sm:col-span-2'
+                    : 'items-center'
+                }`}
+              >
+                <ProjectCard project={project} />
+              </div>
+            ))}
+          </div>
+          <footer className='relative mt-8 mb-0 h-20 rounded-t-full bg-gray-800 py-4 text-center text-white sm:mt-12 sm:py-6'>
+            <p className='text-xs sm:text-sm'>
+              © {new Date().getFullYear()} SHOWZONE WPH BATCH 2. All rights
+              reserved.
+            </p>
+            <p className='text-xs sm:text-sm'>Built with ❤️ by @muhsinsuny.</p>
+          </footer>
         </div>
-        <footer className='relative mt-8 mb-0 rounded-t-full bg-gray-800 py-4 text-center text-white sm:mt-12 sm:py-6'>
-          <p className='text-xs sm:text-sm'>
-            © {new Date().getFullYear()} SHOWZONE WPH BATCH 2. All rights
-            reserved.
-          </p>
-          <p className='text-xs sm:text-sm'>Built with ❤️ by @muhsinsuny.</p>
-        </footer>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,69 +3,68 @@ import React, { useState } from 'react';
 
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div className='custom-container flex-between md:flex-center fixed z-20 flex h-full w-full overflow-hidden bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 md:mb-2 md:px-28'>
-      <nav className='absolute top-0 left-1/2 z-60 h-20 w-full -translate-x-1/2 rounded-b-full border-1 border-amber-200 p-4 text-white shadow-lg sm:mb-8 md:flex md:flex-row md:items-center md:justify-between'>
-        <div className='flex h-20 w-full flex-row items-center justify-between md:px-4'>
-          <div className='top-0 right-0 left-0 z-10 w-full items-center justify-between md:flex'>
-            <div className='text-xl font-bold sm:text-2xl'>
-              <a href='#' className='hover:text-gray-200'>
-                SHOWZONE
-              </a>
-            </div>
-            <button
-              className='block cursor-pointer focus:outline-none sm:hidden'
-              onClick={() => setMenuOpen(!menuOpen)}
-            >
-              <svg
-                className='h-6 w-6'
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                stroke='currentColor'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth={2}
-                  d={
-                    menuOpen
-                      ? 'M6 18L18 6M6 6l12 12'
-                      : 'M4 6h16M4 12h16M4 18h16'
-                  }
-                />
-              </svg>
-            </button>
+    <nav className='custom-container flex-between fixed top-0 left-1/2 z-50 h-20 w-full -translate-x-1/2 rounded-b-full bg-gray-800 bg-gradient-to-r p-4 text-white opacity-80 shadow-2xl sm:mb-8 md:flex md:flex-row md:items-center md:justify-between md:px-28'>
+      <div className='flex-between flex w-full flex-row md:px-4'>
+        <div className='flex w-full items-center justify-between px-4 md:flex'>
+          <div className='text-xl font-bold sm:text-2xl'>
+            <a href='#' className='hover:text-gray-200'>
+              SHOWZONE
+            </a>
           </div>
-          <ul
-            className={`mb:justify-between mb:flex-row mt-4 flex-col space-y-4 text-sm sm:mt-0 sm:flex sm:flex-row sm:space-y-0 sm:space-x-6 sm:text-lg ${
-              menuOpen ? 'flex' : 'hidden'
-            }`}
+          <button
+            className='block cursor-pointer focus:outline-none md:hidden'
+            onClick={() => setMenuOpen(!menuOpen)}
+            aria-label='Toggle menu'
+            type='button'
           >
-            <li>
-              <a href='#' className='hover:text-gray-200'>
-                Home
-              </a>
-            </li>
-            <li>
-              <a href='#projects' className='hover:text-gray-200'>
-                Projects
-              </a>
-            </li>
-            <li>
-              <a href='#about' className='hover:text-gray-200'>
-                About
-              </a>
-            </li>
-            <li>
-              <a href='#contact' className='hover:text-gray-200'>
-                Contact
-              </a>
-            </li>
-          </ul>
+            <svg
+              className='h-6 w-6'
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth={2}
+                d={
+                  menuOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16M4 18h16'
+                }
+              />
+            </svg>
+          </button>
         </div>
-      </nav>
-    </div>
+        <ul
+          className={`mt-4 flex-col space-y-4 text-sm md:mt-0 md:flex md:flex-row md:space-y-0 md:space-x-6 md:text-lg ${
+            menuOpen ? 'flex' : 'hidden'
+          } md:flex`}
+        >
+          <li>
+            <a href='#' className='cursor-pointer hover:text-gray-200'>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href='#projects' className='cursor-pointer hover:text-gray-200'>
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href='#about' className='cursor-pointer hover:text-gray-200'>
+              About
+            </a>
+          </li>
+          <li>
+            <a href='#contact' className='cursor-pointer hover:text-gray-200'>
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
